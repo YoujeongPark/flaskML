fd1fd1b5397a9991b802f388d8a84b2adaef1257
YoujeongPark <53921789+YoujeongPark@users.noreply.github.com>
2020-06-04 15:11:11 +0900

    -
    -

---
+++ project1/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/test')
+def home():
+    return render_template('test.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
