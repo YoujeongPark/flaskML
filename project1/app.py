4102c462bcd259c4c8c2f5b9e65975b059af658a
YoujeongPark <53921789+YoujeongPark@users.noreply.github.com>
2020-06-04 17:42:25 +0900

    framework
    framework ver2

--- project1/app.py
+++ project1/app.py
@@ -8,11 +8,15 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
+
 @app.route('/test')
 def home():
     return render_template('test.html')
 
 
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
